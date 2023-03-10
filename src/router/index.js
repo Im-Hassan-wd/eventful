43a28372cd3d618379d4ddfb9d6b8aca31df1505
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-10 12:50:28 +0100

    defining our routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '../views/About'
 import Home from '../views/Home'
+import Login from '../views/Login'
+import Logout from '../views/Logout'
+import Register from '../views/Register'
+import Create from '../views/Create'
 
 const routes = [
   {
@@ -9,9 +12,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: Logout
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
 ]
 
