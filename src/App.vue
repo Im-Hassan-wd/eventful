812159f02dbf681ebde21324c9d84c20ace5fc20
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-10 15:13:27 +0100

    nav

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
-  <router-view></router-view>
-  <img src="./assets/logo.png" alt="">
+  <Nav />
 </template>
 
 <script>
+import Nav from "./components/Nav"
+
 
 export default {
   name: "App",
-  components: {  }
+  components: { Nav, }
 }
 </script>
 
 <style>
-#app {
-  text-align: center;
-  margin: 64px auto;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-}
-a {
-  color: #000;
-}
-.router-link-active, .router-link-exact-active {
-  color: crimson;
-}
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="bg-teal-600 p-3 px-6 text-white">
+    <h1 class="italic font-bold">Eventful Moments.</h1>
+
+    <ul class="">
+      <span>
+        <router-link to="/">Login</router-link>
+        <router-link to="/">Logout</router-link>
+      </span>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
