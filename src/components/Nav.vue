bc5ac426cb44a0b44b0c12f99109f2a9e3e3580a
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-10 21:36:42 +0100

    styling the nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav class="flex justify-between bg-teal-600 p-3 px-6 text-white">
+  <nav class="flex justify-between bg-teal-600 p-3 px-12 text-white">
     <h1 class="italic font-bold">Eventful Moments.</h1>
 
     <ul class="">
-      <span class="text-sm">
+      <span class="text-sm pl-3">
         <router-link v-if="!login" :to="{ name: 'Login'}">Login</router-link>
       </span>
-      <span class="text-sm">
+      <span class="text-sm pl-3">
         <router-link v-if="login" to="#">Logout</router-link>
       </span>
-      <span class="text-sm">
-        <router-link v-if="!login" to="#">Register</router-link>
+      <span class="text-sm pl-3">
+        <router-link v-if="!login" to="/register">Register</router-link>
       </span>
-      <span class="text-sm">
+      <span class="text-sm pl-3">
         <router-link v-if="login" to="#">My Bucket</router-link>
       </span>
     </ul>
