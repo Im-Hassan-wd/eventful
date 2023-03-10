11f43223084c8dd6ce84c5de06dc5ff14b28dd78
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-10 12:39:56 +0100

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
+  <img src="./assets/logo.png" alt="">
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin: 64px auto;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+a {
+  color: #000;
+}
+.router-link-active, .router-link-exact-active {
+  color: crimson;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 className="text-3xl underline">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
