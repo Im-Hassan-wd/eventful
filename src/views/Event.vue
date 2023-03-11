bf87f524d2c45243c1d23fdcbb15179449d231d2
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-11 11:45:06 +0100

    working on the event page

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -2,9 +2,9 @@
   <Nav />
 
   <div class="px-16">
-    <h1 class="font-bold text-base mb-1 leading-5 mt-10 text-2xl">Lorem ipsum began as scrambled, nonsensical Latin derived from Cicero’s 1st-century.</h1>
-    <small></small>
-    <p class="mb-1 text-sm">
+    <h1 class="font-bold mb-1 font-normal mt-10 text-2xl mb-1">Lorem ipsum began as scrambled, nonsensical Latin derived from Cicero’s 1st-century.</h1>
+    <small class="text-gray-500">10/03/2023</small>
+    <p class="my-12 text-sm leading-5">
       Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.<br/><br/>
       Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.<br/><br/>
       Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.
