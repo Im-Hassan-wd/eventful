20d80999b28ec9156a4d3e2c502421a692ca892b
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-20 08:28:42 +0100

    cleaning up

--- src/views/moments/Home.vue
+++ src/views/moments/Home.vue
@@ -11,22 +11,22 @@
       </router-link>
     </div>
 
-    <div class="my-8 grid lg:grid-cols-2 gap-10">
+    <div class="my-8 grid lg:grid-cols-2 gap-10 mb-24">
       <!-- card -->
-        <div v-for="moment in moments" :key="moment.id" class="shadow p-5 rounded hover:bg-active">
-            <h4 class="font-bold text-base mb-4">{{ moment.title }}</h4>
-            <p class="mb-1 details text-sm leading-7">{{ moment.details }}</p>
-            <div class="mt-4 flex items-center justify-between">
-              <router-link class="grow" :to="{ name: 'Event', params: { id: moment._id } }">
-                <small class="text-secondary">View Details</small>
-              </router-link>
-              <small class="text-gray-300 px-5">{{ moment.createdAt.slice(0, 10) }}</small>
-              <small flex flex-col>{{ moment.updatedAt.slice(0, 10) }}</small>
-            </div>
-        </div>
+      <div v-for="moment in moments" :key="moment.id" class="flex flex-col shadow p-5 rounded hover:bg-active">
+          <h4 class="font-bold text-base mb-4">{{ moment.title }}</h4>
+          <p class="mb-1 details text-sm leading-7">{{ moment.details }}</p>
+          <div class="mt-4 flex items-center justify-between mt-auto">
+            <router-link class="grow mt-4" :to="{ name: 'MomentDetails', params: { id: moment._id } }">
+              <small class="text-secondary">View Details</small>
+            </router-link>
+            <small class="text-gray-300 px-5">{{ moment.createdAt.slice(0, 10) }}</small>
+            <small flex flex-col>{{ moment.updatedAt.slice(0, 10) }}</small>
+          </div>
+      </div>
     </div>
 
-    <div v-if="moments.length > 4" class="text-center my-24">
+    <div v-if="moments.length > 4" class="text-center mb-24">
       <button class="btn bg-secondary">Load More</button>
     </div>
   </div>
